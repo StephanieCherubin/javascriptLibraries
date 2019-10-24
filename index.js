997db46bd687abe431a9984bac5f34eed76399fe
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-24 13:46:09 -0700

    add challenge 5

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ Write a function that uppercases the first character of string */
 const firstUpper = (string) => { // javascript libraries
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
-console.log(firstUpper("hello girl"))
+// console.log(firstUpper("hello girl"))
 
 // Here's another way
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
-console.log("hello boy".capitalize())
+// console.log("hello boy".capitalize())
 
 
 /* Challenge 2 
@@ -18,7 +18,7 @@ allCaps() - makes all characters uppercase */
 const allCaps = (str) => {
   return str.toUpperCase()
 }
-console.log(allCaps("hello lady"))
+// console.log(allCaps("hello lady"))
 
 /* Challenge 3 
 capitalizeWords() - makes the first character of each word uppercase.
@@ -40,15 +40,12 @@ console.log(capitalizeWords("just do it"))
 capitalizeHeadline() - 
 capitalizes all of the words except the articles: the, in, a, an, and, but, for, of, at, by, from */
 
-const arr = "Baklava is an unusual name for a dog".split(" ")
-
-// substringsArray.some(substring=>yourBigString.includes(substring))
-
 const titleCase = (str) => {
-  const arr1 = ["an", "the", "in", "a", "and", "but", "for", "of", "at", "by"]
+  const articles = ["an", "the", "in", "a", "and", "but", "for", "of", "at", "by"]
   return str.toLowerCase().split(' ').map((word)=> {
-    for (let i = 0; i < arr1.length; i =+ 1) {
-      if (arr1[i] === word){
+    for (let i = 0; i < articles.length; i += 1) {
+      
+      if (articles.includes(word)) {
         return word;
       } 
       return word.replace(word[0], word[0].toUpperCase());
@@ -57,4 +54,32 @@ const titleCase = (str) => {
 }
 console.log(titleCase("munchausen syndrome by proxy is an unusual name for a cat"));
 
+/* 
+Challenge 4 
+removeExtraSpaces() - Removes all spaces from the beginning and end of a String along with any extra spaces in the middle. If more than one space appears in the middle of a string it is replaced by a single space.
+*/
+const removeExtraSpaces = (string) => {
+	const wordList = string.split(" ")
+	let finalString = ""
+  wordList.forEach((word, _) => {
+    const result = firstUpper(word)
+    finalString = finalString.concat(result, " ")
+  })
+   
+  const noExtraSpaces = finalString.slice(0, -1).trim()
+  return noExtraSpaces
+}
 
+console.log(removeExtraSpaces(" help me "))
+
+/*
+Challenge 5 kabobCase() - Removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
+ */
+const arrayOfObjects = [
+  { ' ': '-' }
+]
+var sentence1 = 'PROTEIN IS THE BUILDING BLOCK OF THE BODY'
+sentence1 = sentence1.toLowerCase()
+const result1 = arrayOfObjects.reduce((f, s) => `${f}`.replace(Object.keys(s)[0], s[Object.keys(s)[0]]), sentence1)
+
+console.log(result1)
