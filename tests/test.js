cffbc8862f336dec989675c5f5d113096ae15e27
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-01 22:41:10 -0700

    add test to titleCase function

--- tests/test.js
+++ tests/test.js
@@ -3,3 +3,8 @@ const lib = require('../index');
 test('sanity check', () => {
   expect(lib.allCaps('abc')).toBe('ABC');
 });
+
+test('title case', () => {
+
+  expect(titleCase('munchausen syndrome by proxy is an unusual name for a cat')).toBe('Munchausen Syndrome by Proxy is an Unusual Name for a Cat');
+})
