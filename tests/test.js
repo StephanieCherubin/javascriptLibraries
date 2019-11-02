ba580e191f465733b05c17c1559031e26abb1fc6
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-01 23:35:46 -0700

    testing build

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,16 @@
 const lib = require('../index');
 
-// test('allCaps function', () => {
-//   expect(lib.allCaps('abc')).toBe('ABC');
-// });
 
 describe('test suite', () => {
   it('allCaps function', () => {
     expect.assertions(1);
     expect(lib.allCaps('abc')).toBe('ABC');
   });
+
+  it('capitalizeHeadline function', () => {
+    expect.assertions(1);
+    expect(lib.capitalizeHeadline('munchausen syndrome by proxy is an unusual name for a cat')).toBe('Munchausen Syndrome by Proxy is an Unusual Name for a Cat');
+  });
 });
 
-// test('capitalizeHeadline function', () => {
-//   expect(lib.capitalizeHeadline('munchausen syndrome by proxy is an unusual name for a cat')).toBe('Munchausen Syndrome by Proxy is an Unusual Name for a Cat');
-// });
+
