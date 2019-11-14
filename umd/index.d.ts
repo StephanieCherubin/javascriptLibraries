281bcd6f0a77aad7ab94401b86e80cb5307eefb6
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-14 14:56:47 -0800

    tell our library users where to find the TypeScript types

---
+++ umd/index.d.ts
@@ -0,0 +1,8 @@
+declare const firstUpper: (string: any) => any;
+declare const allCaps: (string: any) => any;
+declare const capitalizeWords: (string: any) => string;
+declare const capitalizeHeadline: (string: any) => any;
+declare const removeExtraSpaces: (string: any) => string;
+declare const kabobCase: (string: any) => any;
+declare const snakeCase: (string: any) => any;
+declare const camelCase: (string: any) => any;
