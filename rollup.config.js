0299908c41419be44cdf634f8b475135dbffeafb
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-14 14:55:48 -0800

    add rollup to dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 export default [
   {
     input: 'src/index.js',
-    plugins: [terser()],
+    plugins: [
+      typescript({
+        typescript: require('typescript'),
+      }),
+      terser()],
     output: {
         file: 'umd/stringLibraries.js',
         format: 'umd',
