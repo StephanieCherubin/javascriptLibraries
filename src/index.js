675d7c09f0b2a5e39d44b267b6aa99e18c409246
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-26 20:28:50 -0800

    edit for small syntax errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-/* Challenge 1
+/*
 Uppercases the first character of string */
 var firstUpper = function (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 module.exports.firstUpper = firstUpper;
-/* Challenge 2
+
+
+/* 
 allCaps() - makes all characters uppercase */
 var allCaps = function (string) { return string.toUpperCase(); };
 module.exports.allCaps = allCaps;
-/* Challenge 3
+/*
 capitalizeWords() - makes the first character of each word uppercase.
 Imagine that each word is separated by a space. */
 var capitalizeWords = function (string) {
@@ -21,8 +23,7 @@ var capitalizeWords = function (string) {
     return finalString.slice(0, -1);
 };
 module.exports.capitalizeWords = capitalizeWords;
-/* Advanced
-capitalizeHeadline() -
+/* 
 capitalizes all of the words except the articles: the, in, a, an, and, but, for, of, at, by, from */
 var capitalizeHeadline = function (string) {
     var articles = ['an', 'the', 'in', 'is', 'a', 'and', 'but', 'for', 'of', 'at', 'by'];
@@ -35,8 +36,6 @@ var capitalizeHeadline = function (string) {
 };
 module.exports.capitalizeHeadline = capitalizeHeadline;
 /*
-Challenge 4
-removeExtraSpaces() -
 Removes all spaces from the beginning and end of a String along with any extra spaces in the middle.
 If more than one space appears in the middle of a string it is replaced by a single space.
 */
@@ -51,19 +50,16 @@ var removeExtraSpaces = function (string) {
 };
 module.exports.removeExtraSpaces = removeExtraSpaces;
 /*
-Challenge 5 kabobCase() - Removes extra spaces and replaces spaces with the hyphen
+Removes extra spaces and replaces spaces with the hyphen
 */
 var kabobCase = function (string) { return string.split(' ').join('-'); };
 module.exports.kabobCase = kabobCase;
 /* Challenge 6 snakeCase() - Removes extra space and replaces spaces with an underscore "_", and makes all characters lowercase.
-Example: " what the heck " -> "what_the_heck"
-Strategies:
-Follow the example above and use the '_' instead of the '-'.
 */
 var snakeCase = function (string) { return string.split(' ').join('_').toLowerCase(); };
 module.exports.snakeCase = snakeCase;
 /*
-Challenge 7 camelCase() -
+
 */
 var camelCase = function (string) { return string.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
     return index == 0 ? word.toLowerCase() : word.toUpperCase();
